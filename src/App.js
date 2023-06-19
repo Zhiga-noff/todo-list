1bf50caaf1a9fb11f37239e5f30ffa8eec82c85a
Kirill Zhiganov <zhigano_k.n@mail.ru>
2023-06-19 17:49:48 +0300

    Разукрасил получение из jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import style from './App.module.css';
+import {useEffect, useState} from "react";
 
+const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos'
+
+ async function createNewTask(setTasks)   {
+    try {
+        const response = await fetch(TODOS_URL)
+        const result = await response.json()
+        setTasks(result)
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 export const App = () => {
+const [tasks, setTasks]=useState([])
 
+    useEffect(()=>{
+        createNewTask(setTasks)
+    },[])
 
   return (
     <div className={style.app}>
+      <form action="#" className={style.form}>
+        <p className={style.title}>Тут твои задачи</p>
+        <div className={style.flexField}>
+          <input
+            className={style.field}
+            type="text"
+            name="fieldNewTask"
+            placeholder={'Напишите что вы хотите сделать'}
+          />
+          <button className={style.button}>Добавить задачу</button>
+        </div>
+      </form>
+      <ul className={style.taskList}>
+        {tasks.map(({ id, title }) => (
+          <li className={style.task} key={id}>
+            {title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
